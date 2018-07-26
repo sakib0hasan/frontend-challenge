7299a6f70cd3840342f97e4ebeb1b11317465f07
Sakib Hasan <sakib8899@gmail.com>
2018-07-27 00:06:34 +0600

    Tab's UI completed

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Octicon from 'react-octicon';
 import StatsBox from "./StatsBox";
+import Tabs from "./Tabs";
 
 class Header extends Component {
     render() {
         return (
             <div className="header m-b-20">
-                <div className="container p-t-20 flex flex-row space-between">
+                <div className="container p-t-20 m-b-20 flex flex-row space-between">
                     <div className="flex flex-row">
                         <div>
                             <Octicon name="repo" className="repo-icon"/>
@@ -20,6 +21,9 @@ class Header extends Component {
                         <StatsBox name='Star' count='107,308'/>
                         <StatsBox name='Fork' count='19,132'/>
                     </div>
+                </div>
+                <div className="container">
+                    <Tabs />
                 </div>
             </div>
         );
---
+++ src/Components/Common/Tabs.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import Octicon from 'react-octicon';
+
+class Tabs extends Component {
+    render() {
+        const Tabs = [
+            {
+                Name: "Code",
+                Icon: "code"
+            },
+            {
+                Name: "Issues",
+                Icon: "issue-opened"
+            },
+            {
+                Name: "Pull Requests",
+                Icon: "git-pull-request"
+            },
+            {
+                Name: "Projects",
+                Icon: "list-unordered"
+            },
+            {
+                Name: "Wiki",
+                Icon: "book"
+            },
+            {
+                Name: "Insights",
+                Icon: "graph"
+            }
+        ];
+
+        return (
+            <div className='tabs flex flex-row'>
+                {
+                    Tabs.map(tab => {
+                        return (
+                            <div className={"tab-item " + (tab.Name === 'Issues' ? 'tab-item-selected' : '')}>
+                                <Octicon name={tab.Icon} className={"repo-icon" + (tab.Name === 'Issues' ? 'icon-black' : '')}/>
+                                <span className='p-l-2'>{tab.Name}</span>
+                                {tab.Name === 'Issues' ? <span className="counter m-l-3">387</span> : ''}
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default Tabs;
